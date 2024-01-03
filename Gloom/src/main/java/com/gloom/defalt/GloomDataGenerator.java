4dc4edd5248a5b2250aaf5cede80431c3a42e15a
DefaultCreator0 <defaultgp1@gmail.com>
2024-01-03 18:06:58 -0500

    Initial commit

---
+++ Gloom/src/main/java/com/gloom/defalt/GloomDataGenerator.java
@@ -0,0 +1,11 @@
+package com.gloom.defalt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GloomDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
