94a2eb3713019c1e339eb1f3d4b8bc423658a37f
DefaultCreator0 <defaultgp1@gmail.com>
2024-01-29 20:26:41 -0500

    1-29-24
    Added Sundelion flower generation

--- Gloom/src/main/java/com/defalt/gloom/Gloom.java
+++ Gloom/src/main/java/com/defalt/gloom/Gloom.java
@@ -6,7 +6,13 @@ import com.defalt.gloom.item.ModItemGroups;
 import com.defalt.gloom.item.ModItems;
 import com.defalt.gloom.world.gen.ModWorldGeneration;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static com.defalt.gloom.potion.ModPotions.registerPotions;
@@ -30,6 +36,12 @@ public class Gloom implements ModInitializer {
 		ModWorldGeneration.generateModWorldGen();
 		registerPotions();
 
+		BiomeModifications.addFeature
+				(
+						BiomeSelectors.foundInOverworld(),
+						GenerationStep.Feature.VEGETAL_DECORATION,
+						RegistryKey.of(RegistryKeys.PLACED_FEATURE,new Identifier(Gloom.ModID,"sundelion_placed"))
+				);
 
 	}
 }
